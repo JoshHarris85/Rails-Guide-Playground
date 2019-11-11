902bdd3ca95f3757a3b84995409d100dc991a065
JoshHarris85 <joshua.harris1085@gmail.com>
2019-11-11 17:56:26 -0600

    fix tests with basic auth

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -1,6 +1,18 @@
 require "application_system_test_case"
 
 class ArticlesTest < ApplicationSystemTestCase
+  def authorize
+    username = 'dhh'
+    password = 'secret'
+    if page.driver.browser.respond_to?(:authorize)
+      # When headless
+      page.driver.browser.authorize(username, password)
+    else
+      # When javascript test
+      visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/"
+    end
+  end
+
   test "visiting the index" do
     visit articles_path
 
@@ -8,15 +20,19 @@ class ArticlesTest < ApplicationSystemTestCase
   end
 
   test "creating an article" do
-    visit articles_path
+    visit root_path
+
+    click_on "New Article"
+
+    authorize
 
     click_on "New Article"
 
     fill_in "Title", with: "Creating article"
-    fill_in "Body", with: "Created this article!"
+    fill_in "Text", with: "Created this article!"
 
     click_on "Create Article"
 
-    assert_text "Creating an Article"
+    assert_text "Add a comment"
   end
 end
